44202d26b623546eee0d3df66e481a7988830dfd
Eneas Diaz <eneasdc@gmail.com>
2026-05-22 15:08:32 -0300

    aleatorios para cada figura

--- Form1.cs
+++ Form1.cs
@@ -9,17 +9,28 @@ namespace Figuras
     public partial class Form1 : Form
     {
         Figura[] figuras;
+        // clase random intanciando un objeto random con el nombre random 
+        Random random = new Random();
 
         public Form1()
         {
             InitializeComponent();
             figuras = new Figura[3] 
             {
-                new Circulo(60) { ColorTrazo = Color.Blue },
-                new Rectangulo(30,50) { ColorTrazo = Color.Green },
-                new Cuadrado(45) { ColorTrazo = Color.Red },
+                new Circulo(60) { ColorTrazo = ColorAleatorio() },
+                new Rectangulo(30,50) { ColorTrazo = ColorAleatorio() },
+                new Cuadrado(45) { ColorTrazo = ColorAleatorio() },
             };
 
+        }
+        // Tipo de dato Color viene de la librería System.Drawing de .NET
+        private Color ColorAleatorio()
+        {// no se contempla el ultimo vavor 0-255 metodo propio de la clase random
+            int colorX = random.Next(0, 256);
+            int colorY = random.Next(0, 256);
+            int colorZ = random.Next(0, 256);
+
+            return Color.FromArgb(colorX, colorY, colorZ);
         }
 
         private void button1_Click(object sender, EventArgs e)
